905d23b54636bc26dd4754fb0d2984378235c49a
moshodifimio <marcus@fimio.xyz>
2024-02-15 15:44:30 -0500

    stdout

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import logging
 from fastapi import FastAPI
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
+    logging.info("Hello World endpoint was called")
     return {"message": "Hello World"}
